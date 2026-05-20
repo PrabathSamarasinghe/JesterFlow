3f735d82bca5e71df226174106149221ada1f8f7
Prabath Samarasinghe <prabhaths2001@gmail.com>
2026-05-20 09:09:20 +0530

    Fixed the security issue

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const csurf = require('csurf');
+
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
+app.use(csurf({ cookie: true }));
 // Import routes
 const userRoutes = require('./user');
 app.use('/api/users', userRoutes);
